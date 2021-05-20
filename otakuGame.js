e936ab9ca2691753de3e576b202af9c9b854b6b6
jaewoongRyu <ipimfmw@nate.com>
2021-05-20 23:13:22 +0900

    fix: 여기서 일단 마무리

--- otakuGame.js
+++ otakuGame.js
@@ -25,6 +25,7 @@ const otakuGame = (function () {
     type = "SYSTEM",
     choices = null,
     next = null,
+    talmobeam = false,
   }) {
     this.text = text;
     // type: 'USER', 'SYSTEM'
@@ -37,7 +38,7 @@ const otakuGame = (function () {
         messageContainer.classList.add("user-message");
       }
       if (this.type === "SYSTEM") {
-        await delay(1000);
+        await delay(500);
       }
       if (choices) {
         renderFooter(choices);
@@ -48,6 +49,13 @@ const otakuGame = (function () {
       if (next) {
         // await delay(1000);
         next.render();
+      }
+      if (talmobeam) {
+        await delay(2000);
+        document.body.classList.add('talmo');
+
+        await delay(2000);
+        document.body.classList.remove('talmo');
       }
       document
         .querySelector("#content-container")
@@ -67,30 +75,25 @@ const otakuGame = (function () {
     container.appendChild(footerContainer);
     document.body.appendChild(container);
 
-    aa = new messageBlock({ text: "내 팬따윈 필요없어" });
-    bb = new messageBlock({ text: "난 말 짧은 남자가 좋더라" });
-    kk = new messageBlock({ text: "난 유재웅이다.", type: "USER", next: bb });
+    end = new messageBlock({text: "game over"})
+    aa = new messageBlock({ text: "머리카락만 잘 지켜도 월 700 이득이라고 생각하고 행복하게 사세요~", next: end });
+    bb = new messageBlock({ text: "돈 밝히면 대머리됨 탈모 빔~~~", next: end, talmobeam: true });
+    kk = new messageBlock({ text: "돈이 짱이지!! 1000만원!!!", type: "USER", next: bb });
     qq = new messageBlock({
-      text: "아이유씨 팬이에요!!!",
+      text: "300만원으로 행복하게 살래..",
       type: "USER",
       next: aa,
     });
 
-    pp = new messageBlock({ text: "안녕하세요. 아이유입니다." });
+    p7 = new messageBlock({ text: "세후 월급 1000만원에 평생 대머리로 살기 vs 세후 월급 300만원에 풍성하게 살기", choices: [kk, qq] });
+    p6 = new messageBlock({ text: "가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하가나다라마바사아자차카타파하", next: p7});
+    p5 = new messageBlock({ text: "컨텐츠 양이 화면 크기를 넘어갈 때 스크롤이 잘 되는지 확인하기 위해 뭔가 계속 넣어야해", next: p6 });
+    p4 = new messageBlock({ text: "근데 영 어설픈 게 유튜브에 나오는 싸구려 광고나 어그로 영상같다.", next: p5 });
+    p3 = new messageBlock({ text: "카카오톡 처럼 보이게 하니 제법 깔끔하네", next: p4 });
+    p2 = new messageBlock({ text: "선택지에 따라 다른 내용으로 이어지는 대화를 시도하고싶었어.", next: p3 });
+    p1 = new messageBlock({ text: "커밍아웃 시뮬레이션 따라하기!!", next: p2 });
+    pp = new messageBlock({ text: "안녕", next: p1 });
     pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    pp.render();
-    await pp.render();
-
-    tt = new messageBlock({ text: "이름이 뭐에요?", choices: [kk, qq] });
-    await tt.render();
   };
   return {
     init: init,
